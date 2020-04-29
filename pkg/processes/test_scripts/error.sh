e5bd8aeeea76dd3a8aa19e9460593d532c31fb0c
Dmitry <dymitr.paremski@keep.network>
2020-04-29 19:53:15 +0200

    Adding test scripts for unit tests

---
+++ pkg/processes/test_scripts/error.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo adsf $IS_ERROR
+if $IS_ERROR; then
+    exit 1
+else
+    exit 0
+fi
+
---
+++ pkg/processes/test_scripts/hello.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+hello(){
+  while :; 
+  do 
+    echo "Hello Percona"
+    sleep 1
+  done
+}
+
+hello
+
